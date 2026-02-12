6324ed4526420a0383ec67cb3cb2917b5083a734
Sude Meydan <sudis.meydan@gmail.com>
2026-02-12 16:36:12 +0300

    feat: Frontend temizligi ve Bootstrap kurulumu yapildi

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">Kullanıcı Yönetim Paneli</h2>
+      <hr />
+      
+      {/* İleride Tablo Buraya Gelecek */}
+      <div className="alert alert-info text-center">
+        Henüz veri yok. Birazdan Backend'e bağlanacağız! 🚀
+      </div>
+    </div>
+  );
+}
+
+export default App;
